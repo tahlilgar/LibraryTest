b2156cde225e7d254bc24b35bd181502b588b329
TahlilgarDevelopers <reihane258@gmail.com>
2019-11-14 09:13:00 +0330

    98/08/23

--- commonlibrary/src/main/java/com/tahlilgargroup/commonlibrary/CommonClass.java
+++ commonlibrary/src/main/java/com/tahlilgargroup/commonlibrary/CommonClass.java
@@ -560,6 +560,15 @@ public class CommonClass {
         context.startActivity(i);
     }
 
+    public boolean isValidEmailId(String email){
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
+    }
+
     public String ErrorMessages(int Code) {
         String msg = "";
         switch (Code) {
